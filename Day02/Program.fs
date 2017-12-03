44579d3f0796ecced80de7fa8bf8c4d1b6d0ff61
Kamil Lawniczak <boomsoft@outlook.com>
2017-12-03 06:29:30 +0100

    Day 02 done

--- Day02/Program.fs
+++ Day02/Program.fs
@@ -19,6 +19,21 @@ let calculateChecksum (matrix:seq<seq<int>>) =
     let diffs = matrix |> Seq.map(fun row -> rowDiff(row))
     diffs |> Seq.sum
     
+//Part two
+
+let findEventlyDivine (lst: seq<int>) =
+    let rec doModuloForElem(elem:int, lst:int list)=
+        match lst with
+        | next::rest -> if elem%next=0 then elem/next else doModuloForElem(elem,rest)
+        | [] -> 0
+
+    let rec findInSorted(nums: int list)=
+        match nums with
+        | head::tail -> if doModuloForElem(head,tail)>0 then doModuloForElem(head,tail) else findInSorted(tail)
+        | [] -> 0
+
+    let sorted = lst |> Seq.sortDescending |> Seq.toList
+    findInSorted(sorted)
 
 [<EntryPoint>]
 let main argv = 
@@ -26,5 +41,8 @@ let main argv =
     let result = calculateChecksum data
     printfn "%A" result
 
+    let diffs2 = data |> Seq.map(fun row -> findEventlyDivine(row))
+    let res2 = diffs2 |> Seq.sum
+    printfn "%A" res2
     Console.ReadLine();
     0 // return an integer exit code
