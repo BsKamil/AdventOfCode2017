db6189238cd4f6fa183dd2d16c47bf5612a11de0
Kamil Lawniczak <boomsoft@outlook.com>
2017-12-03 05:47:04 +0100

    day 02 part 1 done

---
+++ Day02/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open FSharp.Data;
+open System;
+
+let readData = 
+    let csv = CsvFile.Load("data.csv", separators=";", hasHeaders=false)
+    let rows = csv.Rows
+
+    rows |> Seq.map(fun r -> r.Columns |> Seq.map(fun col -> Int32.Parse(col)))
+
+let rowDiff (lst: seq<int>) =
+    let min = lst |> Seq.min
+    let max = lst |> Seq.max
+    max-min
+
+let calculateChecksum (matrix:seq<seq<int>>) =
+    let diffs = matrix |> Seq.map(fun row -> rowDiff(row))
+    diffs |> Seq.sum
+    
+
+[<EntryPoint>]
+let main argv = 
+    let data = readData
+    let result = calculateChecksum data
+    printfn "%A" result
+
+    Console.ReadLine();
+    0 // return an integer exit code
