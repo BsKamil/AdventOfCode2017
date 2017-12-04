9753433845b5913e5c099074353ed01d1809314a
Kamil Lawniczak <boomsoft@outlook.com>
2017-12-04 11:52:50 +0100

    Day 04 done

--- Day04/Program.fs
+++ Day04/Program.fs
@@ -8,14 +8,31 @@ let words (str:string) = str.Split(sep)
 let passwordValid (w:string[]) =
      w.Length=(w |> Array.distinct |> Array.length)
 
+//Part 2
+
+let fixSingle(str:string)= 
+    let fix="yyy"
+    if str.Length=1 then sprintf "%s%s" str fix else str
+
+let passValid2 (w:String[])=
+    w.Length=(w |> Array.map(fun str -> str |> Seq.sort |> Seq.toArray |> String)
+                |> Array.distinct |> Array.length)
+
 [<EntryPoint>]
 let main argv = 
     let input = File.ReadAllLines("data.txt")
-    let result = 
+    let result1 = 
         input |> Seq.filter(fun ln -> passwordValid(words ln)) 
         |> Seq.sumBy(fun i -> 1)
     
-    
-    printfn "%A" result
+    printfn "%A" result1
+
+    let result2 = 
+        input |> Seq.filter(fun ln -> passValid2(words ln)) 
+        |> Seq.sumBy(fun i -> 1)
+
+    printfn "%A" result2
+    printfn "%A" (passValid2(words "abcde xyz ecdab"))
+
     Console.ReadLine()
     0 // return an integer exit code
